91f761735c10d8b852a987d2f7bd3d3506ffb4c3
nphadte <nareshphadte@hotmail.com>
2018-11-20 10:46:29 -0500

    Adding DB details

---
+++ src/main/java/com/example/OnlineVideoConverter/OnlineVideoConverterApplication.java
@@ -0,0 +1,13 @@
+package com.example.OnlineVideoConverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineVideoConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineVideoConverterApplication.class, args);
+	}
+	
+}
